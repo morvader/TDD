f1ec8526c05d04c9c62008f217afb0d959ee7708
Francisco Moreno <franciscomoreno@gmail.com>
2012-10-10 23:22:12 +0200

    Subida inicial

---
+++ KatasTDD/KatasTDD/CalculadoraDeCadenas/NUnitCalcuradoraCadenasTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace KatasTDD.CalculadoraDeCadenas
+{
+    [TestFixture]
+    public class NUnitCalcuradoraCadenasTest
+    {
+        #region SetUp
+        [TestFixtureSetUp]
+        public void SetupMethods()
+        {
+        }
+        [TestFixtureTearDown]
+        public void TearDownMethods()
+        {
+        }
+        [SetUp]
+        public void SetupTest()
+        {
+        }
+        [TearDown]
+        public void TearDownTest()
+        {
+        }
+        #endregion
+        
+        [Test]
+        public void MyTest()
+        {
+        }
+    }
+}
