c02b7621c0c83955c6b1c08651ff04c93aee2846
Francisco Moreno <franciscomoreno@gmail.com>
2012-10-14 19:42:10 +0200

    FizzBuzzKata

---
+++ KatasTDD/KatasTDD/FizzBuzz/MyFizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KatasTDD.FizzBuzz
+{
+    class MyFizzBuzz
+    {
+        internal static string[] getValues(int valorLimite)
+        {
+            List<string> resultado = new List<string>();
+
+            string valorIntroducir;
+
+            for (int i = 1; i <= valorLimite; i++)
+            {
+                valorIntroducir = i.ToString();
+
+                if (i % 3 == 0 && i % 5 == 0)
+                    valorIntroducir = "FizzBuzz";
+                else if (i % 3 == 0)
+                    valorIntroducir = "Fizz";
+                else if (i % 5 == 0)
+                    valorIntroducir = "Buzz";
+                
+                resultado.Add(valorIntroducir);
+            }
+
+            return resultado.ToArray();
+        }
+    }
+}
---
+++ KatasTDD/KatasTDD/FizzBuzz/NUnitFizzBuzzTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace KatasTDD.FizzBuzz
+{
+    [TestFixture]
+    public class NUnitFizzBuzzTest
+    {
+
+        #region Setup
+        [TestFixtureSetUp]
+        public void SetupMethods()
+        {
+           
+        }
+        [TestFixtureTearDown]
+        public void TearDownMethods()
+        {
+        }
+        [SetUp]
+        public void SetupTest()
+        {
+        }
+        [TearDown]
+        public void TearDownTest()
+        {
+        }
+        #endregion
+
+        [Test]
+        public void getFizzBuzzHasta10()
+        {
+            var fizzBuzz = new MyFizzBuzz();
+
+            string[] resultado = MyFizzBuzz.getValues(10);
+            string[] esperado = new string[] {"1","2","Fizz", "4", "Buzz","Fizz","7","8","Fizz","Buzz"};
+
+            CollectionAssert.AreEqual(esperado, resultado);
+        }
+
+        [Test]
+        public void getFizzBuzzHasta15()
+        {
+            var fizzBuzz = new MyFizzBuzz();
+
+            string[] resultado = MyFizzBuzz.getValues(15);
+            string[] esperado = new string[] { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz","11","Fizz","13","14","FizzBuzz" };
+
+            CollectionAssert.AreEqual(esperado, resultado);
+        }
+    }
+}
