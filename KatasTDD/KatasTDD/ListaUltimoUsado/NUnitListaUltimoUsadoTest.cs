34ee577ee34b2a44ee193568400408c0afb62d8e
Francisco Moreno <franciscomoreno@gmail.com>
2012-10-17 22:51:59 +0200

    Lista ultimo usado subida inicial

---
+++ KatasTDD/KatasTDD/ListaUltimoUsado/NUnitListaUltimoUsadoTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace KatasTDD.ListaUltimoUsado
+{
+    [TestFixture]
+    public class NUnitListaUltimoUsadoTest
+    {
+        [TestFixtureSetUp]
+        public void SetupMethods()
+        {
+        }
+        [TestFixtureTearDown]
+        public void TearDownMethods()
+        {
+        }
+        [SetUp]
+        public void SetupTest()
+        {
+        }
+        [TearDown]
+        public void TearDownTest()
+        {
+        }
+        [Test]
+        public void inicialmenteVacia()
+        {
+            TListaUltimoUsado lista = new TListaUltimoUsado();
+
+            Assert.That(lista.Count(), Is.EqualTo(0), string.Format("El tamño inicial debe ser 0 cuando en realidad es: [{0}]", lista.Count()));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage="No se permiten inserciones vacias")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void NoInsercionesVacias(string elemento)
+        {
+            TListaUltimoUsado lista = new TListaUltimoUsado();
+            lista.Add(elemento);
+        }
+
+        [Test]
+        public void comprobarPrimerElementoEsUltimoInsertado()
+        {
+            var lista = new TListaUltimoUsado();
+            lista.Add("Primero");
+            lista.Add("Segundo");
+            lista.Add("Tercero");
+
+            string primeroEsperado = "Tercero";
+            string primeroLista = lista.First();
+
+            Assert.That(primeroEsperado, Is.EqualTo(primeroLista), string.Format("El primera de la lista [{0}] y el esperado [{1}] no coinciden",primeroLista, primeroEsperado));
+        }
+    }
+}
---
+++ KatasTDD/KatasTDD/ListaUltimoUsado/TListaUltimoUsado.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KatasTDD.ListaUltimoUsado
+{
+    class TListaUltimoUsado: IEnumerable<string>
+    {
+        #region privateMembers
+        //Internamente implementamos una lista
+        List<string> _lista;
+        #endregion
+
+        #region publicMembers
+
+        public TListaUltimoUsado()
+        {
+            _lista = new List<string>();
+        }
+
+        public void Add(string p)
+        {
+            //Comprobar que los elementos a introducir son validos
+            comprobarElemento(p);
+
+            //Añadir elementos al principio
+            _lista.Insert(0, p);
+        }
+
+        private void comprobarElemento(string p)
+        {
+            if (string.IsNullOrEmpty(p)) throw new ArgumentException("No se permiten inserciones vacias");
+        }
+
+        #endregion
+
+        #region privateMembers
+
+        #endregion
+
+
+        #region IEnumerable<string> Members
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return _lista.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+       
+    }
+}
